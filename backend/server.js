8c59c4269cd15a21cc46c85ab87e164acd88cb68
fulei888 <fulei888@churchofjesuschrist.org>
2020-11-01 23:24:11 -0700

    make it better

--- backend/server.js
+++ backend/server.js
@@ -29,18 +29,13 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 })
 app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
-// app.get("/api/products/:id", (req, res) => {
-//   const productId = req.params.id;
-//   const product = data.products.find(x => x._id === productId);
-//   if (product)
-//     res.send(product);
-//   else
-//     res.status(404).send({ msg: "Product Not Found." })
-// });
-
-// app.get("/api/products", (req, res) => {
-//   res.send(data.products);
-// });
+app.use((err, req, res, next) => {
+  res.status(500).send({message: err.message})
+});
 
 app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
