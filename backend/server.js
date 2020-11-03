e026a95a272ae9a175d41f680e652df6eb24504c
fulei888 <fulei888@churchofjesuschrist.org>
2020-11-02 23:25:06 -0700

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -38,5 +38,5 @@ app.get('*', (req, res) =>
 app.use((err, req, res, next) => {
   res.status(500).send({message: err.message})
 });
-
+const port = process.env.PORT || 5000;
 app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
