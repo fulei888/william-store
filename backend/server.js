5dab5fec86fd52fe233a0f02722d87694bd24cde
fulei888 <fulei888@churchofjesuschrist.org>
2020-11-03 20:29:37 -0700

    publish

--- backend/server.js
+++ backend/server.js
@@ -39,4 +39,4 @@ app.use((err, req, res, next) => {
   res.status(500).send({message: err.message})
 });
 const port = process.env.PORT || 5000;
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+app.listen(5000, () => { console.log("Server started at http://localhost:port") });
