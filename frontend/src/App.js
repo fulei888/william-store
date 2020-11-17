e68d20ba4a756b07731d6a1eda7e0b4c213da04c
fulei888 <fulei888@churchofjesuschrist.org>
2020-11-16 22:33:03 -0700

    go

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ function App() {
         <Link to="/">William's Store</Link>
       </div>
       <div className="header-links">
-        <a href="cart.html">Cart</a>
+        <a href="/cart/cart.html">Cart</a>
         {
           userInfo ? <Link to ="/profile">{userInfo.name}</Link>
           : <Link to="/signin">Sign In</Link>
